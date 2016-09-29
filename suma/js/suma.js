9848aafca02f74216fec2288b894838020f47a4c
alba <albafgfoto@gmail.com>
2016-09-29 22:47:52 +0200

    Ejercicio Suma

---
+++ suma/js/suma.js
@@ -0,0 +1,13 @@
+
+var pideNumero = "Escribe un número";
+var numero1;
+var numero2;
+var suma = numero1 + numero2;
+
+
+
+numero1 = window.prompt(pideNumero);
+numero2 = window.prompt(pideNumero);
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
+window.alert("El resultado es: " + suma);
