c780dd3f09e79540f6e417bdae1b069c87d12b99
alba <albafgfoto@gmail.com>
2016-09-29 18:20:49 +0200

    Ahora sí

--- saludar/js/saludar.js
+++ saludar/js/saludar.js
@@ -1,6 +1,3 @@
-const GREETING_ES = "Escriba su nombre ",
-      GREETING_EN = "Give me your name ",
-      SALUTE_EN = "Good afternoon ",
-      SALUTE_ES = "Buenas tardes ";
+
 var name = window.prompt(GREETING_ES);
 window.alert(SALUTE_ES + " " + name);
